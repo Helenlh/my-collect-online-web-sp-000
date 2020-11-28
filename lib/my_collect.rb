183a93bae59b9314e43ccb2f3aec90b62e18a2fe
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-28 18:46:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,9 +6,10 @@ def my_collect(array)
     while i < array.length 
       value = yield array[i]
       puts value
-   i += 1 
+   i += 1
+   
  end 
-   results << value 
+  
 end 
 return results 
 end 
